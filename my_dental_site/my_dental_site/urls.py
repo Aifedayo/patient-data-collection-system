34e0ac3f64c6e3da15f913eb828fb8416d503ab2
akeem lagundoye <akeemifedayolag@gmail.com>
2022-05-13 12:23:39 +0100

    media static

--- my_dental_site/my_dental_site/urls.py
+++ my_dental_site/my_dental_site/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('office_app/', include('office_app.urls')),
+    path('', RedirectView.as_view(url='office_app/')),
 ]
