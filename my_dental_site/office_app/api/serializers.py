783d61a4b136566c8a284ec7cbb96283e6e17849
akeem lagundoye <akeemifedayolag@gmail.com>
2022-05-14 10:49:35 +0100

    serializers.py

--- my_dental_site/office_app/api/serializers.py
+++ my_dental_site/office_app/api/serializers.py
@@ -1,1 +1,17 @@
+from random import choices
 from rest_framework import serializers
+
+GENDER_CHOICES = (
+        ('Female', 'Female'),
+        ('Male', 'Male')
+    )
+class PatientSerializer(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    age = serializers.IntegerField()
+    email = serializers.EmailField()
+    phone_number = serializers.CharField()
+    timestamp = serializers.DateTimeField(read_only=True)
+    gender = serializers.CharField(choices=GENDER_CHOICES)
+    avatar = serializers.ImageField()
+    is_active = serializers.BooleanField()
