7a7e5fb29841b9541997131706ed350838480291
akeem lagundoye <akeemifedayolag@gmail.com>
2022-05-15 14:22:59 +0100

    validation code

--- my_dental_site/office_app/api/serializers.py
+++ my_dental_site/office_app/api/serializers.py
@@ -1,18 +1,31 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from django.utils.timesince import timesince
 from rest_framework import serializers
 from office_app.models import Patient, Doctors
 
 class PatientSerializer(serializers.ModelSerializer):
 
-    #time_since_admitted = serializers.SerializerMethodField()
+    time_since_admitted = serializers.SerializerMethodField()
 
     class Meta:
         model = Patient
         exclude = ['assigned_doctor']
 
-    # def get_time_since_admitted(self, object):
-    #     timestamp = object.timestamp
-    #     now = datetime.now()
-    #     time_delta = timesince(timestamp, now)
-    #     return time_delta
+    def get_time_since_admitted(self, object):
+        timestamp = object.timestamp
+        print(timestamp)
+        now = datetime.now(timezone.utc)
+        time_delta = timesince(timestamp, now)
+        return time_delta
+
+    # Define an Object layer validation
+    def validate(self, data):
+        if data['age'] < 0:
+            raise serializers.ValidationError('Age cannot be a negative value!!!')
+        return data
+
+    def validate_phone_number(self, value):
+        if len(value) < 11:
+            raise serializers.ValidationError('Phone number cannot be lesser then 11 digits!!!')
+        return value
+    
